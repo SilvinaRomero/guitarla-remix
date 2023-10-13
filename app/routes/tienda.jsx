4ec2a35b97c12f070b197ad5f07f934dd8a914dd
SilvinaRomero <sromero.fernandez@gmail.com>
2023-10-13 21:07:45 +0200

    guitarra component

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,8 +1,32 @@
+import { getGuitarras } from "../models/guitarras.server"
+import { useLoaderData } from "@remix-run/react"
+import Guitarra from "../components/guitarra"
+
+
+export async function loader() {
+    const guitarras = await getGuitarras()
+    return guitarras.data
+}
+
+
 function Tienda() {
+    const guitarras = useLoaderData();
+    // console.log(guitarras)
     return (
-        <div>
-            tiendas
-        </div>
+        <main className="contenedor">
+            <h2  className="heading">Nuestra Colección</h2>
+
+            {guitarras.length && (
+                <div className="guitaras-grid">
+                    {guitarras.map(guitarra => (
+                        <Guitarra 
+                            key={guitarra?.id}
+                            guitarra={guitarra}
+                        />
+                    ))}
+                </div>
+            )}
+        </main>
     )
 }
 
