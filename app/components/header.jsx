1966b144c782c27a6ebb60b31e6105f3629bb3c3
SilvinaRomero <sromero.fernandez@gmail.com>
2023-10-13 19:13:40 +0200

    navigation

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,7 +1,9 @@
-import { Link } from "@remix-run/react"
+import { Link, useLocation } from "@remix-run/react"
 import Logo from '../../public/img/logo.svg'
 
 function Header() {
+    const location = useLocation()
+    // console.log(location)
     return (
         <header className="header">
             <div className="contenedor barra">
@@ -11,15 +13,19 @@ function Header() {
                 <nav className="navegacion">
                     <Link
                         to="/"
+                        className={location.pathname === '/' ? 'active' : ''}
                     >Inicio</Link>
                     <Link
                         to="/nosotros"
+                        className={location.pathname === '/nosotros' ? 'active' : ''}
                     >Nosotros</Link>
                     <Link
                         to="/tienda"
+                        className={location.pathname === '/tienda' ? 'active' : ''}
                     >Tienda</Link>
                     <Link
                         to="/blog"
+                        className={location.pathname === '/blog' ? 'active' : ''}
                     >Blog</Link>
                 </nav>
             </div>
