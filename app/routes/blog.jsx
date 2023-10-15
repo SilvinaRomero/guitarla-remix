5208577cd1591a6b7e3e324d3dde5ffdb44b06c0
SilvinaRomero <sromero.fernandez@gmail.com>
2023-10-15 13:10:02 +0200

    blog page

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,8 +1,51 @@
+import { useLoaderData } from "@remix-run/react";
+import { getPosts } from "~/models/post.server"
+import styles from "~/styles/blog.css"
+import Post from "../components/post";
+
+
+export async function loader() {
+    const posts = await getPosts();
+
+    return posts.data
+}
+
+export function meta() {
+    return ([
+        {
+            title: `GuitarLA - Blog`,
+        },
+        {
+            description: 'GitarLA - Blog de música y venta de guitarras'
+        }
+    ])
+}
+
+export function links() {
+    return [
+        {
+            rel: 'stylesheet',
+            href: styles
+        }
+    ]
+}
+
 function Blog() {
+    const posts = useLoaderData()
+    // console.log(posts)
     return (
-        <div>
-            este es el blog
-        </div>
+        <main className="contenedor altura">
+            <h2 className="heading">Blog</h2>
+
+            <div className="blog">
+                {posts.map(post => (
+                    <Post
+                        key={post.id}
+                        post={post}
+                    />
+                ))}
+            </div>
+        </main>
     )
 }
 
